f3888bd37d3b3f8cb1b8017577afb82a04d489dd
Jordan Fisher <thementalgoose@gmail.com>
2020-04-27 10:59:30 +0100

    Updated version to 1.1 (2) and updated release notes

--- app/src/main/java/tmg/hourglass/extensions/CountdownTypeExtensions.kt
+++ app/src/main/java/tmg/hourglass/extensions/CountdownTypeExtensions.kt
@@ -9,5 +9,7 @@ fun CountdownType.label(): Int {
         CountdownType.MONEY_GBP -> R.string.countdown_type_money_gbp
         CountdownType.MONEY_USD -> R.string.countdown_type_money_usd
         CountdownType.MONEY_EUR -> R.string.countdown_type_money_eur
+        CountdownType.MILES -> R.string.countdown_type_miles
+        CountdownType.KILOMETRES -> R.string.countdown_type_kilometres
     }
 }
