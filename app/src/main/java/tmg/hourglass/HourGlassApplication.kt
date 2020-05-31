1b4470c2dfeb6ba9f8a2f29a76feb5f69c934ce4
Jordan Fisher <thementalgoose@gmail.com>
2020-05-31 21:40:14 +0100

    Bumped version to 2.2, added updated date for widgets

--- app/src/main/java/tmg/hourglass/HourGlassApplication.kt
+++ app/src/main/java/tmg/hourglass/HourGlassApplication.kt
@@ -16,6 +16,7 @@ import tmg.hourglass.prefs.PreferencesManager
 import tmg.hourglass.realm.RealmDBMigration
 
 val releaseNotes: Map<Int, Int> = mapOf(
+    9 to R.string.release_9,
     8 to R.string.release_8,
     7 to R.string.release_7,
     6 to R.string.release_6,
