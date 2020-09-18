542817276e7f9b26e196c272929f3b2a4bc0ace3
Jordan Fisher <thementalgoose@gmail.com>
2020-09-18 19:08:55 +0100

    Updated version to 2.7.0

--- app/src/main/java/tmg/hourglass/HourGlassApplication.kt
+++ app/src/main/java/tmg/hourglass/HourGlassApplication.kt
@@ -16,6 +16,7 @@ import tmg.hourglass.prefs.PreferencesManager
 import tmg.hourglass.realm.RealmDBMigration
 
 val releaseNotes: Map<Int, Int> = mapOf(
+    15 to R.string.release_15,
     14 to R.string.release_14,
     13 to R.string.release_13,
     12 to R.string.release_12,
