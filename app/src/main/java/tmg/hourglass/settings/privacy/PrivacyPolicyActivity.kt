e3a33596aef6c65df7a4ec195095f733fed98627
Jordan Fisher <thementalgoose@gmail.com>
2022-01-12 10:03:22 +0000

    Migrates Privacy Policy over to using compose

--- app/src/main/java/tmg/hourglass/settings/privacy/PrivacyPolicyActivity.kt
+++ app/src/main/java/tmg/hourglass/settings/privacy/PrivacyPolicyActivity.kt
@@ -2,28 +2,27 @@ package tmg.hourglass.settings.privacy
 
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
+import androidx.activity.compose.setContent
 import org.koin.android.viewmodel.ext.android.viewModel
 import tmg.hourglass.R
 import tmg.hourglass.base.BaseActivity
 import tmg.hourglass.databinding.ActivityPrivacyPolicyBinding
+import tmg.hourglass.presentation.AppTheme
 import tmg.utilities.extensions.fromHtml
 import tmg.utilities.extensions.observeEvent
 
 class PrivacyPolicyActivity: BaseActivity() {
 
-    private lateinit var binding: ActivityPrivacyPolicyBinding
     private val viewModel: PrivacyPolicyViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityPrivacyPolicyBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.tvPolicy.text = getString(R.string.privacy_policy_data).fromHtml()
-        binding.tvPolicy.movementMethod = LinkMovementMethod.getInstance()
-
-        binding.ibtnClose.setOnClickListener {
-            viewModel.inputs.clickBack()
+        setContent {
+            AppTheme {
+                PrivacyPolicyLayout(
+                    backClicked = viewModel.inputs::clickBack
+                )
+            }
         }
 
         observeEvent(viewModel.outputs.goBack) {
---
+++ app/src/main/java/tmg/hourglass/settings/privacy/PrivacyPolicyLayout.kt
@@ -0,0 +1,61 @@
+package tmg.hourglass.settings.privacy
+
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
+import tmg.hourglass.R
+import tmg.hourglass.presentation.AppTheme
+import tmg.hourglass.presentation.AppThemePreview
+import tmg.hourglass.presentation.layouts.TitleBar
+
+@Composable
+fun PrivacyPolicyLayout(
+    backClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState)
+    ) {
+        TitleBar(
+            title = stringResource(id = R.string.settings_help_privacy_policy_title),
+            backClicked = backClicked
+        )
+        Box(modifier = Modifier.fillMaxSize()
+            .padding(
+                start = AppTheme.dimensions.paddingMedium,
+                end = AppTheme.dimensions.paddingMedium,
+                bottom = AppTheme.dimensions.paddingLarge
+            )) {
+            AndroidView(factory = { context ->
+                TextView(context).apply {
+                    text = HtmlCompat.fromHtml(context.getString(R.string.privacy_policy_data), HtmlCompat.FROM_HTML_MODE_LEGACY)
+                    movementMethod = LinkMovementMethod.getInstance()
+                }
+            })
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    AppThemePreview(isLight = true) {
+        PrivacyPolicyLayout(
+            backClicked = { }
+        )
+    }
+}
