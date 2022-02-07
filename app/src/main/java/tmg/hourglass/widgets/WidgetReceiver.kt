495eae13f55855674463c47fc8da6b188d89412c
Jordan Fisher <thementalgoose@gmail.com>
2022-02-07 17:43:54 +0000

    WIP adding jetpack compose widgets

--- app/src/main/java/tmg/hourglass/widgets/WidgetReceiver.kt
+++ app/src/main/java/tmg/hourglass/widgets/WidgetReceiver.kt
@@ -1,5 +1,7 @@
 package tmg.hourglass.widgets
 
+import android.appwidget.AppWidgetManager
+import android.content.Context
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import tmg.hourglass.widgets.layout.CountdownWidget
 
--- app/src/main/java/tmg/hourglass/widgets/layout/CountdownWidget.kt
+++ app/src/main/java/tmg/hourglass/widgets/layout/CountdownWidget.kt
@@ -2,8 +2,10 @@ package tmg.hourglass.widgets.layout
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceModifier
 import androidx.glance.LocalSize
 import androidx.glance.appwidget.GlanceAppWidget
@@ -11,12 +13,16 @@ import androidx.glance.appwidget.SizeMode
 import androidx.glance.background
 import androidx.glance.extractModifier
 import androidx.glance.layout.*
+import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
+import androidx.glance.text.TextAlign
+import androidx.glance.unit.ColorProvider
+import org.koin.core.component.getScopeId
+import java.lang.reflect.Modifier
 
 class CountdownWidget(
     private val countdownId: String
 ): GlanceAppWidget() {
-
 
     companion object {
         private val SMALL_BOX = DpSize(90.dp, 90.dp)
@@ -60,27 +66,125 @@ private fun Box() {
 
 @Composable
 private fun Row() {
-    Row(modifier = GlanceModifier
+    Column(modifier = GlanceModifier
         .fillMaxSize()
-        .padding(8.dp)
     ) {
-        val modifier = GlanceModifier.fillMaxSize().padding(4.dp).defaultWeight()
-        (1..6).forEach {
-            Box(
-                modifier = modifier
-                    .background(if (it % 2 == 0) Color.White else Color.Red)) {
-
-            }
+        Row {
+            Text(
+                text = "countdown-id",
+                style = androidx.glance.text.TextStyle(
+                    fontSize = 20.sp,
+                    color = ColorProvider(Color.White)
+                )
+            )
+        }
+        Row(
+            modifier = GlanceModifier.fillMaxWidth()
+        ) {
+            Text(
+                modifier = GlanceModifier.defaultWeight(),
+                text = "0",
+                style = androidx.glance.text.TextStyle(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = ColorProvider(Color.White)
+                )
+            )
+            Text(
+                modifier = GlanceModifier.defaultWeight(),
+                text = "10000",
+                style = androidx.glance.text.TextStyle(
+                    fontSize = 16.sp,
+                    textAlign = TextAlign.End,
+                    fontWeight = FontWeight.Bold,
+                    color = ColorProvider(Color.White)
+                )
+            )
+        }
+        Row(modifier = GlanceModifier
+            .fillMaxSize()
+        ) {
+            ProgressBar(
+                progress = 0.6f,
+                barColor = Color.Green,
+                backgroundColor = Color.White
+            )
         }
     }
 }
 
 @Composable
 private fun Column() {
-    Row(modifier = GlanceModifier
+    Column(modifier = GlanceModifier
         .fillMaxSize()
-        .background(Color.Red)
     ) {
-        Text("COLUMN")
+        ProgressBar(
+            progress = 0.6f,
+            barColor = Color.Blue
+        )
+    }
+}
+
+
+
+private const val defaultGranularity = 10
+
+@Composable
+private fun RowScope.ProgressBar(
+    progress: Float,
+    barColor: Color,
+    modifier: GlanceModifier = GlanceModifier,
+    backgroundColor: Color = Color.Transparent,
+    granularity: Int = defaultGranularity
+) {
+    for (x in 0 until granularity) {
+        val boxThreshold = x.toFloat() / granularity.toFloat()
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+                .defaultWeight()
+                .background(
+                    when (progress) {
+                        1f -> barColor
+                        0f -> backgroundColor
+                        else -> {
+                            if (boxThreshold <= progress) {
+                                barColor
+                            } else {
+                                backgroundColor
+                            }
+                        }
+                    })
+        ) { }
+    }
+}
+
+@Composable
+private fun ColumnScope.ProgressBar(
+    progress: Float,
+    barColor: Color,
+    modifier: GlanceModifier = GlanceModifier,
+    backgroundColor: Color = Color.Transparent,
+    granularity: Int = defaultGranularity
+) {
+    (granularity..1).forEach {
+        val boxThreshold = it / granularity
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+                .defaultWeight()
+                .background(
+                    when (progress) {
+                        1f -> barColor
+                        0f -> backgroundColor
+                        else -> {
+                            if (boxThreshold <= progress) {
+                                barColor
+                            } else {
+                                backgroundColor
+                            }
+                        }
+                    })
+                ) { }
     }
 }
