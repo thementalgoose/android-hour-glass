39c12ab0e4b01cbdf41d29a2f41737a5238aca56
Jordan Fisher <thementalgoose@gmail.com>
2022-08-13 21:58:28 +0100

    Deletes old layout files

--- app/src/main/java/tmg/hourglass/settings/SettingsModel.kt
+++ app/src/main/java/tmg/hourglass/settings/SettingsModel.kt
@@ -1,18 +1,15 @@
 package tmg.hourglass.settings
 
-import androidx.annotation.LayoutRes
 import androidx.annotation.StringRes
-import tmg.hourglass.R
 
 sealed class SettingsModel(
-    @LayoutRes val layoutId: Int,
     val id: Int
 ) {
 
     data class Header(
         @StringRes
         val title: Int
-    ): SettingsModel(R.layout.element_settings_category, id = title)
+    ): SettingsModel(id = title)
 
     data class SwitchPref(
         @StringRes
@@ -21,7 +18,7 @@ sealed class SettingsModel(
         val description: Int,
         val getState: () -> Boolean,
         val saveState: (value: Boolean) -> Unit
-    ): SettingsModel(R.layout.element_settings_preference_switch, id = title) {
+    ): SettingsModel(id = title) {
         var initialState: Boolean = getState()
     }
 
@@ -31,5 +28,5 @@ sealed class SettingsModel(
         @StringRes
         val description: Int,
         val onClick: (() -> Unit)?
-    ): SettingsModel(R.layout.element_settings_preference, id = title)
+    ): SettingsModel(id = title)
 }
