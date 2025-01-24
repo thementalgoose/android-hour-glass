828fd1ea7c31e11fc59aee28b61d45a7833d4fdc
Jordan Fisher <thementalgoose@gmail.com>
2025-01-24 00:13:41 +0000

    Countdown test updates

--- domain/src/test/java/tmg/hourglass/domain/model/CountdownTest.kt
+++ domain/src/test/java/tmg/hourglass/domain/model/CountdownTest.kt
@@ -11,26 +11,15 @@ import tmg.hourglass.domain.enums.CountdownType.NUMBER
 internal class CountdownTest {
 
     @Test
-    fun `Countdown startByType and endByType returns localdate at end of value when type is days`() {
+    fun `Countdown startAtStartOfDay and endAtStartOfDay returns start of day value`() {
 
         val start = LocalDateTime.of(2020, 1, 1, 1, 1)
-        val end = LocalDateTime.of(2020, 1, 2, 1, 1)
+        val end = LocalDateTime.of(2020, 1, 2, 23, 1)
         val countdown = Countdown(id = "", name = "", description = "", colour = "", start = start, end = end, startValue = "0", endValue = "1", countdownType = DAYS, interpolator = LINEAR, notifications = emptyList())
 
-        val expectedStart = LocalDateTime.of(2020, 1, 1, 23, 59, 59)
-        val expectedEnd = LocalDateTime.of(2020, 1, 2, 23, 59, 59)
-        assertEquals(expectedStart, countdown.startByType)
-        assertEquals(expectedEnd, countdown.endByType)
-    }
-
-    @Test
-    fun `Countdown startByType and endByType returns regular value when type is not days`() {
-
-        val start = LocalDateTime.of(2020, 1, 1, 1, 1)
-        val end = LocalDateTime.of(2020, 1, 2, 1, 1)
-        val countdown = Countdown(id = "", name = "", description = "", colour = "", start = start, end = end, startValue = "0", endValue = "1", countdownType = NUMBER, interpolator = LINEAR, notifications = emptyList())
-
-        assertEquals(start, countdown.startByType)
-        assertEquals(end, countdown.endByType)
+        val expectedStart = LocalDateTime.of(2020, 1, 1, 0, 0, 0, 0)
+        val expectedEnd = LocalDateTime.of(2020, 1, 2, 0, 0, 0, 0)
+        assertEquals(expectedStart, countdown.startAtStartOfDay)
+        assertEquals(expectedEnd, countdown.endAtStartOfDay)
     }
 }
